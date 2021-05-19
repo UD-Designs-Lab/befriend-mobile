25f70ef0e381ed802a498dfcd4b8aafc1fb01117
matrixjnr <jonnyeinstein@gmail.com>
2021-05-19 23:39:25 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# befriend
+
+Let's connect.
+
+## Getting Started
+
+This is befirend's mobile app repository.
---
+++ android/app/src/main/kotlin/io/simplyvirtual/befriend/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.simplyvirtual.befriend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
